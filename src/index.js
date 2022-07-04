d3c921cc716553b9959cc8f5436a57c44d2c858a
AnishTiwari1531 <anishtiwari1531@gmail.com>
2022-07-04 13:27:53 +0530

    Final push

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require("multer")
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(multer().any());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 mongoose.connect("mongodb+srv://Anish_Tiwari1531:SINGH1531@cluster0.40jpapr.mongodb.net/Group8Database?retryWrites=true&w=majority", {
